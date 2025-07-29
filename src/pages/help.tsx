166670051ce91cc62c5a3c690a5fdba613fd8d0e
SoRyeong Kwon <etrhffma4567@gmail.com>
2025-07-29 13:14:45 +0900

    도움 요청 페이지 생성

---
+++ src/pages/help.tsx
@@ -0,0 +1,12 @@
+import styles from "@/styles/Help.module.scss";
+import classNames from 'classnames/bind';
+
+const cn = classNames.bind(styles);
+
+export default function Help(){
+    return (
+        <div>
+
+        </div>
+    )
+} 
