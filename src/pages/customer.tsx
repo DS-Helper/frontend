b28f8d694784ffe4800d6158878d1724c5238000
SoRyeong Kwon <etrhffma4567@gmail.com>
2025-08-20 17:29:24 +0900

    작업 중: 문의 내역 탭

--- src/pages/customer.tsx
+++ src/pages/customer.tsx
@@ -1,11 +1,22 @@
+// 고객 문의 페이지
 import styles from '@/styles/Customer.module.scss';
 import classNames from 'classnames/bind';
+import { time } from 'console';
 import React, { useState } from "react";
 
 const cn = classNames.bind(styles);
 
 export default function Customer(){
     const [activeTab, setActiveTab] = useState<"history" | "register">('history');
+    const [inquiryType, setInquiryType] = useState("");
+    const [content, setContent] = useState("");
+    const [images, setImages] = useState<File[]>([]);
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        // API 요청이나 처리 로직 추가하기
+        console.log({ inquiryType, content, images });
+    }
     return (
         <div className={cn("customer")}>
             <h1 className={cn("title")}>고객 문의</h1>
@@ -33,9 +44,17 @@ export default function Customer(){
                 )}
 
                 {activeTab === "register" && (
-                    <form className={cn("registerFrom")}>
+                    <form className={cn("registerFrom")} onSubmit={handleSubmit}>
                         <div className={cn("formGroup")}>
                             <label>문의 유형</label>
+                            <select 
+                                value={inquiryType}
+                                onChange={(e) => setInquiryType(e.target.value)}>
+                                    <option value="help">도움 요청</option>
+                                    <option value="uncomfortable">서비스 이용 불편</option>
+                                    <option value="modify">서비스 개선 제안</option>
+                                    <option value="etc">기타</option>
+                                </select>
                         </div>
                     </form>
 
