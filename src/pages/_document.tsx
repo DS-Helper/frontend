c8fb48ad17a9ed2592cf84b27748e02e9ccb2e8e
juahcheon <juacheon@gmail.com>
2025-10-23 20:43:33 +0900

    feat: 메타태그 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="eoPfzkPLZSthrx3DHXwYrKawOdwP3iHOnMNUGwQAjb4" />
+      </Head>
       <body>
         <Main />
         <NextScript />
