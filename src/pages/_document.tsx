e283b66ca79346b670255168ae632bd8c3f8b9df
이윤호 <134574255+dldbsgh00005@users.noreply.github.com>
2026-02-02 20:12:29 +0900

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,13 +4,11 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
-        <!-- Google Tag Manager -->
         <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-P6VCF2RG');</script>
-        <!-- End Google Tag Manager -->
         {/* 기본 META 태그 */}
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
@@ -21,10 +19,8 @@ export default function Document() {
         <link rel="sitemap" type="application/xml" href="/sitemap.xml" />
       </Head>
       <body>
-        <!-- Google Tag Manager (noscript) -->
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P6VCF2RG"
         height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
         <Main />
         <NextScript />
       </body>
