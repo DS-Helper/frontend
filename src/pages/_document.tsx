df3eb6932bb2db018a64e20e4c6f5d907ad60a00
이윤호 <134574255+dldbsgh00005@users.noreply.github.com>
2026-02-02 20:25:51 +0900

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ export default function Document() {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-XXXX');
+})(window,document,'script','dataLayer','GTM-W9SFJS2J');
             `,
           }}
         />
