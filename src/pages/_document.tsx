3e524b53afe0b35a1619259ce7dee1d8531756d1
이윤호 <134574255+dldbsgh00005@users.noreply.github.com>
2026-02-02 20:12:04 +0900

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
     <Html lang="ko">
       <Head>
         {/* GTM */}
-          <!-- Google tag (gtag.js) -->
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-FFMWJLG1E4"></script>
           <script>
             window.dataLayer = window.dataLayer || [];
@@ -14,13 +13,11 @@ export default function Document() {
           
             gtag('config', 'G-FFMWJLG1E4');
           </script>
-        <!-- Google Tag Manager -->
         <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-W9SFJS2J');</script>
-        <!-- End Google Tag Manager -->
         {/* 기본 META 태그 */}
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
@@ -32,10 +29,8 @@ export default function Document() {
       </Head>
       <body>
         {/* GTM */}
-        <!-- Google Tag Manager (noscript) -->
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W9SFJS2J"
         height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
         <Main />
         <NextScript />
       </body>
