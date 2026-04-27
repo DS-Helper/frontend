4c1b13c0e47c115940c32b87ffb0bef423a36810
juahcheon <juacheon@gmail.com>
2026-04-27 20:34:23 +0900

    feat: footer sns icon 추가

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,5 +1,6 @@
 import styles from "@/styles/Footer.module.scss";
 import classNames from "classnames/bind";
+import Image from "next/image";
 import Link from "next/link";
 
 const cn = classNames.bind(styles);
@@ -8,31 +9,47 @@ export default function Footer() {
   return (
     <footer className={cn("footer")}>
       <div className={cn("footerContainer")}>
-        <p className={cn("footerTitle")}>운영자 정보</p>
-        <ul className={cn("footerInfo")}>
-          <li>DS Helper 팀</li>
-          <li>대표: 신인호</li>
-        </ul>
-        <p className={cn("footerTitle")}>연락처</p>
-        <ul className={cn("footerInfo", "footerInfoContact")}>
-          <div className={cn("footerInfoContactItem")}>
-            <li className={cn("footerInfoContactItemTitle")}>전화번호 : 010-5250-9548</li>
-            <li>이메일 : dshelper77@gmail.com</li>
-          </div>
-          <div className={cn("footerInfoContactItem")}>
-            <li>인스타그램 : <a href="https://www.instagram.com/ds_helper_?igsh=bGRtN3pkNG95MnB1&utm_source=qr" target="_blank" rel="noopener noreferrer">https://www.instagram.com/ds_helper_?igsh=bGRtN3pkNG95MnB1&utm_source=qr</a></li>
-          </div>
-          <div>
-            <li>네이버 블로그 : <a href="https://blog.naver.com/dshelper" target="_blank" rel="noopener noreferrer">https://blog.naver.com/dshelper</a></li>
-          </div>
-        </ul>
-        <div className={cn("footerLinkContainer")}>
-          <Link href="/terms" className={cn("footerLink")}>이용약관</Link>
-          <Link href="/privacy" className={cn("footerLink")}>개인정보처리방침</Link>
+        <div className={cn("socialLinkContainer")}>
+          <Link
+            href="https://blog.naver.com/dshelper"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="네이버 블로그 바로가기"
+            className={cn("socialLink")}
+          >
+            <Image src="/naver_Icon.svg" alt="네이버 아이콘" width={48} height={48} />
+          </Link>
+          <Link
+            href="https://www.instagram.com/ds_helper_?igsh=bGRtN3pkNG95MnB1&utm_source=qr"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="인스타그램 바로가기"
+            className={cn("socialLink")}
+          >
+            <Image src="/Instagram_Icon.svg" alt="인스타그램 아이콘" width={48} height={48} />
+          </Link>
         </div>
-        <span className={cn("footerCopyright")}>
-          © 2025 DS Helper. All rights reserved.
-        </span>
+        <div className={cn("footerInfoContainer")}>
+          <p className={cn("footerTitle")}>운영자 정보</p>
+          <ul className={cn("footerInfo")}>
+            <li>DS Helper 팀</li>
+            <li>대표: 신인호</li>
+          </ul>
+          <p className={cn("footerTitle")}>연락처</p>
+          <ul className={cn("footerInfo", "footerInfoContact")}>
+            <div className={cn("footerInfoContactItem")}>
+              <li className={cn("footerInfoContactItemTitle")}>전화번호 : 010-5250-9548</li>
+              <li>이메일 : dshelper77@gmail.com</li>
+            </div>
+          </ul>
+          <div className={cn("footerLinkContainer")}>
+            <Link href="/terms" className={cn("footerLink")}>이용약관</Link>
+            <Link href="/privacy" className={cn("footerLink")}>개인정보처리방침</Link>
+          </div>
+          <span className={cn("footerCopyright")}>
+            © 2025 DS Helper. All rights reserved.
+          </span>
+        </div>
       </div>
     </footer>
   );
